b35c49c8c60f87b69418377e732d06a3c9a2b5a0
malfuncti0n <savvaniss@yahoo.gr>
2017-03-28 11:48:35 +0300

    add a getuser function for login

--- utils/routes.js
+++ utils/routes.js
@@ -1,169 +1,191 @@
 /*
-* Angular 2 CRUD application using Nodejs
-* @autthor Shashank Tiwari
-*/
+ * Angular 2 CRUD application using Nodejs
+ * @autthor Shashank Tiwari
+ */
 
 'use strict';
 const helper = require("./helper");
 
-class Routes{
+class Routes {
 
-	constructor(app){
-		this.app = app;
-	}
-
-
-	/* creating app Routes starts */
-	appRoutes(){
+    constructor(app) {
+        this.app = app;
+    }
 
 
-		/* Route to get all users starts*/
-		this.app.get('/api/users',(request,response) =>{
-
-			helper.getUsers( (result) => {
-				if (result) {
-					response.status(200).json({
-						users:result
-					});
-				}else{
-					response.status(404).json({
-						message:`No usres found.`
-					});
-				}
-			});
-		});
-		/* Route to get all users ends*/
+    /* creating app Routes starts */
+    appRoutes() {
 
 
-		/* Route to add new user starts*/
-		this.app.post('/api/users/',(request,response) =>{
+        /* Route to get all users starts*/
+        this.app.get('/api/users', (request, response) => {
+
+            helper.getUsers((result) => {
+                if (result) {
+                    response.status(200).json({
+                        users: result
+                    });
+                } else {
+                    response.status(404).json({
+                        message: `No user found.`
+                    });
+                }
+            });
+        });
+        /* Route to get all users ends*/
 
 
-				helper.addUser( request.body , (result)=>{
-
-					if (result.error) {
-
-						response.status(403).json({
-							error : true,
-							message : `Error.`
-						});
-
-					} else{
-
-						helper.getUsers( (result) => {
-							if (result) {
-								response.status(200).json({
-									error : false,
-									users:result
-								});
-							}else{
-								response.status(404).json({
-									error : true,
-									message:`No usres found.`
-								});
-							}
-						});
-					};
-				});
-		});
-		/* Route to add new user ends*/
+        /* Route to add new user starts*/
+        this.app.post('/api/users/', (request, response) => {
 
 
-		/* Route to delete user starts*/
-		this.app.delete('/api/users/:id',(request,response) =>{
+            helper.addUser(request.body, (result) => {
 
-			if (request.params.id && request.params.id!='') {
+                if (result.error) {
 
-				helper.removeUsers( request.params.id, (result)=>{
+                    response.status(403).json({
+                        error: true,
+                        message: `Error.`
+                    });
 
-					if (result.error) {
+                } else {
 
-						response.status(403).json({
-							error : true,
-							message : `Error.`
-						});
-
-					} else{
-
-						helper.getUsers( (result) => {
-							if (result) {
-								response.status(200).json({
-									error : false,
-									users:result
-								});
-							}else{
-								response.status(404).json({
-									error : true,
-									message:`No usres found.`
-								});
-							}
-						});
-
-
-					};
-
-				});
-
-			}else{
-				response.status(403).json({
-					error : true,
-					message : `Invalid user Id.`
-				});
-			}
-		});
-		/* Route to delete user ends*/
+                    helper.getUsers((result) => {
+                        if (result) {
+                            response.status(200).json({
+                                error: false,
+                                users: result
+                            });
+                        } else {
+                            response.status(404).json({
+                                error: true,
+                                message: `No users found.`
+                            });
+                        }
+                    });
+                };
+            });
+        });
+        /* Route to add new user ends*/
 
 
-		/* Route to update user starts*/
-		this.app.put('/api/users/:id',(request,response) =>{
+        /* Route to delete user starts*/
+        this.app.delete('/api/users/:id', (request, response) => {
+
+            if (request.params.id && request.params.id != '') {
+
+                helper.removeUsers(request.params.id, (result) => {
+
+                    if (result.error) {
+
+                        response.status(403).json({
+                            error: true,
+                            message: `Error.`
+                        });
+
+                    } else {
+
+                        helper.getUsers((result) => {
+                            if (result) {
+                                response.status(200).json({
+                                    error: false,
+                                    users: result
+                                });
+                            } else {
+                                response.status(404).json({
+                                    error: true,
+                                    message: `No usres found.`
+                                });
+                            }
+                        });
 
 
-			if (request.params.id && request.params.id!='') {
+                    };
 
-				helper.updateUser( request.params.id, request.body , (result)=>{
+                });
 
-					if (result.error) {
-
-						response.status(403).json({
-							error : true,
-							message : `Error.`
-						});
-
-					} else{
-
-
-						helper.getUsers( (result) => {
-							if (result) {
-								response.status(200).json({
-									error : false,
-									users:result
-								});
-							}else{
-								response.status(404).json({
-									error : true,
-									message:`No usres found.`
-								});
-							}
-						});
+            } else {
+                response.status(403).json({
+                    error: true,
+                    message: `Invalid user Id.`
+                });
+            }
+        });
+        /* Route to delete user ends*/
 
 
-					};
+        /* Route to update user starts*/
+        this.app.put('/api/users/:id', (request, response) => {
 
-				});
 
-			}else{
-				response.status(403).json({
-					error : true,
-					message : `Invalid user Id.`
-				});
-			}
-		});
-		/* Route to update user ends*/
+            if (request.params.id && request.params.id != '') {
 
-	}
+                helper.updateUser(request.params.id, request.body, (result) => {
 
-	routesConfig(){
-		this.appRoutes();
-	}
+                    if (result.error) {
+
+                        response.status(403).json({
+                            error: true,
+                            message: `Error.`
+                        });
+
+                    } else {
+
+
+                        helper.getUsers((result) => {
+                            if (result) {
+                                response.status(200).json({
+                                    error: false,
+                                    users: result
+                                });
+                            } else {
+                                response.status(404).json({
+                                    error: true,
+                                    message: `No usres found.`
+                                });
+                            }
+                        });
+
+
+                    };
+
+                });
+
+            } else {
+                response.status(403).json({
+                    error: true,
+                    message: `Invalid user Id.`
+                });
+            }
+        });
+        /* Route to update user ends*/
+
+        /* Route to authenticate user*/
+        this.app.post('/api/authenticate', (request, response) => {
+
+
+            helper.getUser(request.body, (result) => {
+
+                if (result) {
+                    response.status(200).json({
+                        user: result
+                    });
+                } else {
+                    response.status(404).json({
+                        message: `No user found.`
+                    });
+                }
+            });
+        });
+        /* Route to aauthenticate user ends*/
+
+
+
+
+    }
+
+    routesConfig() {
+        this.appRoutes();
+    }
 }
 module.exports = Routes;
